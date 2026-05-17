9aa3872081dc8cc6b6e1626807e1d44fd2a6474d
guud-stewdint <guud.student@techie.com>
2026-05-17 16:55:28 -0700

    Create Room entities and mapper for Habit management

---
+++ data/ActivityItem.kt
@@ -0,0 +1,9 @@
+package com.nightowlcrew.nudgie.data
+
+data class ActivityItem(
+    val id: Int,
+    val icon: String,
+    val description: String,
+    val time: String,
+    val isCompleted: Boolean
+)
