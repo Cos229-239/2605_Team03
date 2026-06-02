efe661140a877c0361a8bd6687fc3d95158b7091
Bailey Kimmel <Baileykimmel756@gmail.com>
2026-06-01 22:48:56 -0500

    Added compose imports

--- app/src/main/java/com/nightowlcrew/nudgie/services/NudgieOverlayService.kt
+++ app/src/main/java/com/nightowlcrew/nudgie/services/NudgieOverlayService.kt
@@ -8,6 +8,11 @@ import android.graphics.PixelFormat
 import android.os.IBinder
 import android.view.Gravity
 import android.view.WindowManager
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+
 
 class NudgieOverlayService : Service()
 {
